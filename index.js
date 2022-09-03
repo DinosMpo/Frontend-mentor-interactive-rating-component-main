01482aca77ea84cb559d9965433df25db0a571fc
DinosMpo <dinos7591@yahoo.gr>
2022-09-03 14:12:57 +0300

    final 2 steps

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 "use strict";
 let ratings = document.getElementsByClassName("rate");
+let selectedRate;
 
 for(let i=0; i<ratings.length; i++) {
 	ratings[i].addEventListener('click', () => {
@@ -10,6 +11,20 @@ for(let i=0; i<ratings.length; i++) {
 	    	ratings[i].className += " active";
 		}
     	console.log(ratings[i].className);
-
+    	selectedRate = ratings[i].innerHTML;
+    	console.log(selectedRate);
 	});
 }
+
+let ratingState = document.getElementById("container-rating-state");
+let thankYouState = document.getElementById("container-thank-you-state");
+let submit = document.getElementById("submit-rating");
+let submitedRating = document.getElementById("submited-rating");
+
+submit.addEventListener("click", () => {
+	if(selectedRate) {
+		submitedRating.innerHTML = "You selected " + selectedRate + " out of 5";		
+		ratingState.style.display = "none";
+		thankYouState.style.display = "block";
+	}
+});
